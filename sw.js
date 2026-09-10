c48df8486e9f043c510f30e39e6e26fea4567198
gouna4 <94395758+gouna4@users.noreply.github.com>
2026-09-10 21:41:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'klusboek-v15';
+var CACHE = 'klusboek-v16';
 var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 var WACHT = 2000; /* zo lang wachten we op het netwerk voordat we de opgeslagen versie pakken */
 
